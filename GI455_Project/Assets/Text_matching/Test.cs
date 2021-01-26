8a95b794d6a29d27ce60278f68e4ee0b383f504a
DESKTOP-PA7OIFK\teame <pakphum.kumj@bumail.net>
2021-01-26 16:17:37 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Text_matching/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public GameObject income, gotcha;
+    public string[] Booker;
+    public string numberone;
+
+    public void Komadikap() 
+    {
+        numberone = income.GetComponent<Text>().text;
+        for (int i = 0; i < Booker.Length; i++) 
+        {
+            if (numberone == Booker[i])
+            {
+                gotcha.GetComponent<Text>().text = "[" + numberone + " ]" + "is found.";
+                break;
+
+            }
+            else 
+            {
+                gotcha.GetComponent<Text>().text = "[" + numberone + " ]" + "is not found.";
+
+            }
+        
+        }
+
+
+    }
+
+    
+    
+}
